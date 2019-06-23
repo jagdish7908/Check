319f6708157a033f1e3e077f8bd9d5de2ebece0b
jagdish7908 <jchaudhary7908@gmail.com>
2019-06-23 19:47:03 +0530

    release 1.0.0.npm init

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const vsts = require("azure-devops-node-api"); // To simplify making REST API calls to Azure Repos
 const bodyParser = require('body-parser'); //  To help parse the JSON returned by the service hook
+var helper = require('./helper_functions');
 
 app.use(bodyParser.json());
 // set up the details for a connection to your account, and get an instance of the Git API
@@ -25,34 +26,31 @@ app.listen(3000, function () {
 });
 
 app.post("/", function (req, res) {
-
     // Get the details about the PR from the service hook payload
     var repoId = req.body.resource.repository.id;
     var pullRequestId = req.body.resource.pullRequestId;
-    var title = req.body.resource.title;
+    var description = req.body.resource.description;
 
     // Build the status object that we want to post.
     var prStatus = {
         "state": "failed",
-        "description": "Ready for review",
-        "targetUrl": "http://visualstudio.microsoft.com",
+        "description": "Checklist",
+        //"targetUrl": "http://visualstudio.microsoft.com",
         "context": {
             "name": "checklist-checker",
             "genre": "continuous-integration"
         }
-    }
+    };
 
-    // Check the title to see if there is "WIP" in the title.
-    if (title.includes("WIP")) {
-        // If so, change the status to pending and change the description.
-        prStatus.state = "pending";
-        prStatus.description = "Work in progress"
+    // Check the description to see if there are mandatory keywords
+    if (helper.keywordCheck(description)) {
+        // If so, change the status to succeeded and change the description.
+        prStatus.state = "succeeded";
+        prStatus.description = "Checklist"
     }
-
     // Post the status to the PR
     vstsGit.createPullRequestStatus(prStatus, repoId, pullRequestId).then( result => {
         console.log(result);
-    });
-
+    }); 
     res.send("Received the POST");
 });
